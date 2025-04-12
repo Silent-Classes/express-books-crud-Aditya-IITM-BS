05e70d5d2c69f507b3a4fc866e9fbc838b5f1fcf
Aditya-IITM-BS <adityabaxla@study.iitm.ac.in>
2025-04-12 10:25:58 +0530

    finished assignment

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,6 +1,21 @@
-const { resetBooks } = require("../data/bookData");
+const { resetBooks, getBooks, addBook } = require("../data/bookData");
 
 // Add controller functions for handling book-related requests
+exports.getBooks = (req, res) => {
+  res.json(getBooks());
+};
+
+exports.addBook = (req, res) => {
+  const { title, author } = req.body;
+
+  if (!title || !author) {
+    return res.status(400).json({
+      error: "Both title and author are required.",
+    });
+  }
+  const book = addBook(req.body);
+  res.status(201).json(book);
+};
 
 // Only for testing, export resetBooks
 exports._resetBooks = resetBooks;
--- src/data/bookData.js
+++ src/data/bookData.js
@@ -2,8 +2,18 @@ let books = [];
 
 // Add function here
 
+function addBook(book) {
+  const newBook = { id: books.length + 1, ...book };
+  books.push(newBook);
+  return newBook;
+}
+
+function getBooks() {
+  return books;
+}
+
 function resetBooks() {
   books = [];
 }
 
-module.exports = { resetBooks };
+module.exports = { resetBooks, getBooks, addBook };
